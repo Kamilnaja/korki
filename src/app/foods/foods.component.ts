2c0b98326d6282c7c0950fca1c1961d187d2bfd1
KAMIL.NAJA <KAMIL.NAJA@EXTERNAL.NEXIGROUP.COM>
2023-04-29 08:45:51 +0200

    Reactive forms

--- src/app/foods/foods.component.ts
+++ src/app/foods/foods.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { FoodsState } from './foods.state';
 
 @Component({
@@ -8,8 +9,14 @@ import { FoodsState } from './foods.state';
 })
 export class FoodsComponent implements OnInit {
   response$ = this.state.foods$;
+  foodForm = this.fb.group({
+    name: ['Cottage cheese'],
+    id: ['1'],
+    caloriesPer100g: [100],
+    weight: [100],
+  });
 
-  constructor(private state: FoodsState) {}
+  constructor(private state: FoodsState, private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.state.getFoods();
