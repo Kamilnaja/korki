677b66940e7a7db32ccbd612150a941fc7978c14
KAMIL.NAJA <KAMIL.NAJA@EXTERNAL.NEXIGROUP.COM>
2023-05-05 16:36:16 +0200

    post request

--- src/app/foods/foods.component.ts
+++ src/app/foods/foods.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { Food } from './foods.model';
 import { FoodsState } from './foods.state';
 
 @Component({
@@ -15,7 +16,7 @@ export class FoodsComponent implements OnInit {
     caloriesPer100g: [100],
     weight: [100],
     nutriScore: ['A'],
-    tags: [1],
+    tags: ['1'],
   });
 
   constructor(private state: FoodsState, private fb: FormBuilder) {}
@@ -25,6 +26,18 @@ export class FoodsComponent implements OnInit {
   }
 
   onSubmit(): void {
-    console.log(this.foodForm.value);
+    const food: Food = this.mapFormToFood();
+    this.state.postFood(food);
+  }
+
+  private mapFormToFood(): Food {
+    return {
+      name: this.foodForm.value.name!,
+      id: this.foodForm.value.id!,
+      caloriesPer100g: this.foodForm.value.caloriesPer100g!,
+      weight: this.foodForm.value.weight!,
+      nutriScore: this.foodForm.value.nutriScore!,
+      tags: this.foodForm.value.tags!,
+    };
   }
 }
