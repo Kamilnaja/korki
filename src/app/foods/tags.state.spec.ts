bbdd671bbea8514a098817c2fd7e57591c9eecbc
KAMIL.NAJA <KAMIL.NAJA@EXTERNAL.NEXIGROUP.COM>
2023-07-01 16:28:37 +0200

    tests using fake async

--- src/app/foods/tags.state.spec.ts
+++ src/app/foods/tags.state.spec.ts
@@ -1,8 +1,8 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync } from '@angular/core/testing';
 import { TagsState } from './tags.state';
 import { MockProvider } from 'ng-mocks';
 import { TagsService } from './tags.service';
-import { TagsResponse } from './tags.model';
+import { Tag, TagsResponse } from './tags.model';
 import { of } from 'rxjs';
 
 describe('TagsState', () => {
@@ -37,12 +37,32 @@ describe('TagsState', () => {
     expect(tagsState).toBeTruthy();
   });
 
-  it('should have value from service', (done) => {
+  xit('should have value from service', (done) => {
     tagsState.tags$.subscribe((tags) => {
       expect(tags).toEqual(tagsResponse.data);
+      // It’s a marker for test runners not to finish the test until we call it.
       done();
     });
 
     tagsState.getTags();
   });
+
+  it('should have value from service', () => {
+    const res: Tag[] = [];
+    tagsState.tags$.subscribe((tags) => {
+      res.push(...tags);
+    });
+
+    tagsState.getTags();
+
+    expect(res).toEqual(tagsResponse.data);
+  });
+
+  it('should have value from service', fakeAsync(() => {
+    tagsState.tags$.subscribe((res) => {
+      expect(res).toEqual(tagsResponse.data);
+    });
+
+    tagsState.getTags();
+  }));
 });
